302800ede1a88977fa9520548f741d0d9e59a349
B Laureen <beffylaureen@me.com>
2023-08-03 17:17:40 -0500

    commit after session with tutor

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+USE employees_db;
+
+INSERT INTO department (name)
+VALUES 
+  ("apparel"),
+  ("cosmetics"),
+  ("shoes");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ("floor manager", 10000.00, 3),
+  ()
